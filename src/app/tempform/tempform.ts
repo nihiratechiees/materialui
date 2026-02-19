ed55dcb7c98675ce73fbb54e8d275ead5fdaded9
nihiratechiees <nihiratechiees@gmail.com>
2026-02-19 12:26:36 +0530

    TDF

--- src/app/tempform/tempform.ts
+++ src/app/tempform/tempform.ts
@@ -1,10 +1,38 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+export interface User {
+  firstname: string;
+  lastname: string;
+  email: string;
+  gender: string;
+  address:string;
+}
+
 @Component({
   selector: 'app-tempform',
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './tempform.html',
   styleUrl: './tempform.css',
 })
+
+
 export class Tempform {
+
+  userobj: User = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    gender: '',
+    address:''
+  }
+
+  OnSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    }
+
+  }
 
 }
