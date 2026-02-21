d946d698f4fca709ab5b4f5fd8ab2ea95e7d3483
nihiratechiees <nihiratechiees@gmail.com>
2026-02-21 19:10:13 +0530

    Register design

---
+++ src/app/Model/usermodel.ts
@@ -0,0 +1,7 @@
+export interface usermodel {
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+    isconfirm: boolean;
+}
--- src/app/register-tdf/register-tdf.ts
+++ src/app/register-tdf/register-tdf.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { usermodel } from '../Model/usermodel';
 
 @Component({
   selector: 'app-register-tdf',
-  imports: [],
+  imports: [MatCardModule,FormsModule,MatFormFieldModule,MatInputModule,
+    MatCheckboxModule,MatButtonModule
+  ],
   templateUrl: './register-tdf.html',
   styleUrl: './register-tdf.css',
 })
 export class RegisterTDF {
+  title='User Registration Form'
+  userform: usermodel={
+    firstname:'',
+    lastname:'',  
+    email:'',
+    password:'',
+    isconfirm:false
+  }
+
+  Login(){
+
+  }
+
+  save(form:NgForm) {
+    // if (this._form.valid) {
+    //   let _data: associateModel = {
+    //     id: this._form.value.id as number,
+    //     name: this._form.value.name as string,
+    //     address: this._form.value.address as string,
+    //     creditlimit: this._form.value.cl as number,
+    //     status: this._form.value.status as boolean
+    //   }
+    //   if (this.isadd) {
+    //     this.service.Create(_data).subscribe(item => {
+    //       alert('Saved.')
+    //       this.close();
+    //     })
+    //   } else {
+    //     _data.id=this._form.getRawValue().id;
+    //     this.service.Update(_data).subscribe(item => {
+    //       alert('Upated.')
+    //       this.close();
+    //     })
+    //   }
+
+    // }
+  }
+
+
 
 }
